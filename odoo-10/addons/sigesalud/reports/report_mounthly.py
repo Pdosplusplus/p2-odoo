4d05a1ef91b286c3ff153714543b5b31b2d34980
Victor Pino <victopin0@gmail.com>
2017-07-24 18:07:23 -0400

    update(sigesalud): Add information of the beneficiary and titular in the report mounthly

--- odoo-10/addons/sigesalud/reports/report_mounthly.py
+++ odoo-10/addons/sigesalud/reports/report_mounthly.py
@@ -27,70 +27,58 @@ class ReportMounthly(models.AbstractModel):
         month = data['form'].get('month')
         selection = data['form'].get('selection')
         cooperative = data['form'].get('cooperative')
-        titular = data['form'].get('titular')
+        titulars = data['form'].get('titular')
         beneficiary = data['form'].get('beneficiary')
 
         #Vars for the control of the data
         info['month'] = month
         info['cooperative'] = cooperative
-        info['titular'] = titular
+        info['titular'] = titulars
         info['beneficiary'] = beneficiary
         info['date'] = datetime.now().date() 
 
         cooperatives = {}
         cooperatives["name"] = cooperative
-        cooperatives["beneficiaries"] = ""
-        cooperatives["num_aso"] = 0
-        cooperatives["events_aso"] = 0
-        cooperatives["num_no_aso"] = 0
-        cooperatives["events_no_aso"] = 0
-        cooperatives["mount_total"] = 0
-        cooperatives["mount_aso"] = 0
-        cooperatives["mount_no_aso"] = 0
-        cooperatives["percentage_aso"] = 0.0
-        cooperatives["percentage_no_aso"] = 0.0
-        cooperatives["events_total"] = 0
-
+        cooperatives["num_titu"] = 0
+        cooperatives["num_beneficiaries"] = 0
+        cooperatives["num_events"] = 0
+        cooperatives["type_events"] = 0
+        cooperatives["total_events"] = 0
+        cooperatives["mount"] = 0
+        cooperatives["porcentage_use"] = 0 
 
         #Vars to show in the report
-        titulars = {}
-        titulars["name"] = ""
-        titulars["num_aso"] = 0
-        titulars["events_aso"] = 0
-        titulars["num_no_aso"] = 0
-        titulars["events_no_aso"] = 0
-        titulars["mount_total"] = 0
-        titulars["mount_aso"] = 0
-        titulars["mount_no_aso"] = 0
-        titulars["percentage_aso"] = 0.0
-        titulars["percentage_no_aso"] = 0.0
-        titulars["events_total"] = 0
+        titular = {}
+        titular["name"] = ""
+        titular["cooperative"] = 0
+        titular["num_events"] = 0
+        titular["type_events"] = 0
+        titular["total_events"] = 0
+        titular["mount"] = 0
+        titular["porcentage_use"] = 0
 
         beneficiaries = {}
         beneficiaries["name"] = ""
-        beneficiaries["num"] = 0
-        beneficiaries["events"] = 0
-        beneficiaries["events_total"] = 0
+        beneficiaries["cooperative"] = ""
+        beneficiaries["relationship"] = ""
+        beneficiaries["num_events"] = 0
+        beneficiaries["type_events"] = 0
+        beneficiaries["total_events"] = 0
         beneficiaries["mount"] = 0
-        beneficiaries["percentage"] = 0
+        beneficiaries["porcentage_use"] = 0
+
+        events = []
 
         if cooperative:
 
-            if cooperative:
-                #Get all expedient    
-                all_expedient = self.env['sigesalud.expedient'].search([('cooperative', '=', cooperative)])
+            #Get all expedient    
+            all_expedient = self.env['sigesalud.expedient'].search([('cooperative', '=', cooperative)])
+
+            cooperatives["num_titu"] = len(all_expedient)
 
             for expedient in all_expedient:
 
-                if expedient.associated == 'si':
-                    
-                    cooperatives["num_aso"] += 1
-        
-                else:
-
-                    cooperatives["num_no_aso"] += 1
-
-                cooperatives["events_total"] += len(expedient.event_ids)
+                cooperatives["total_events"] += len(expedient.event_ids)
 
                 #Recorremos los eventos realizados por el titular
                 for event in expedient.event_ids:
@@ -98,47 +86,62 @@ class ReportMounthly(models.AbstractModel):
                     #Verificamos si el mes del evento es igual al seleccionado.
                     if compareMounts(month, event.date):
 
-                        if expedient.associated == 'si':
-                    
-                            cooperatives["events_aso"] += 1
-                            cooperatives["mount_aso"] += int(event.cost)
+                        cooperatives["num_events"] += 1
+                        cooperatives["mount"] += float(event.cost)
 
-                        else:
+                        if not event.type_event in events:
 
-                            cooperatives["events_no_aso"] += 1
-                            cooperatives["mount_no_aso"] += int(event.cost)
-
-                beneficiaries["events_total"] += len(expedient.beneficiary_ids)
-
+                            events.append(event.type_event)
+                        
                 for beneficiary in expedient.beneficiary_ids:
+
+                    cooperatives["total_events"] += len(expedient.event_ids)
 
                     for event in beneficiary.event_ids:
 
                         #Verificamos si el mes del evento es igual al seleccionado.
                         if compareMounts(month, event.date):
 
-                            beneficiaries["events"] += 1
-                            beneficiaries["mount"] += int(event.cost)
+                            cooperatives["num_events"] += 1
+                            cooperatives["mount"] += float(event.cost)
 
-            if cooperatives["events_total"] > 0:
+                            if not event.type_event in events:
 
-                cooperatives["percentage_aso"] = cooperatives["events_aso"] * 100 / cooperatives["events_total"]
-                cooperatives["percentage_no_aso"] = cooperatives["events_no_aso"] * 100 / cooperatives["events_total"]
+                                events.append(event.type_event)
 
-            cooperatives["mount_total"] = cooperatives["mount_aso"] + cooperatives["mount_no_aso"]
+            if cooperatives["total_events"] > 0:
 
-            if beneficiaries["events_total"] > 0:
+                cooperatives["porcentage_use"] = cooperatives["num_events"] * 100 / cooperatives["total_events"]
 
-                beneficiaries["percentage"] = beneficiaries["events"] * 100 / beneficiaries["events_total"]
-
-            cooperatives["beneficiaries"] = beneficiaries
+            cooperatives["type_events"] = events
             info['cooperative'] = cooperatives
         
-        if titular:
+        if titulars:
 
-            all_expedient = self.env['sigesalud.expedient'].search([('id', '=', titular[0])])
-            titulars["name"] = titular[1]
+            expedient = self.env['sigesalud.expedient'].search([('id', '=', titulars[0])])
+            
+            titular["name"] = titulars[1]
+            titular["cooperative"] = expedient.cooperative
+            titular["total_events"] = len(expedient.event_ids)
 
+            for event in expedient.event_ids:
+
+                #Verificamos si el mes del evento es igual al seleccionado.
+                if compareMounts(month, event.date):
+
+                    titular["num_events"] += 1
+                    titular["mount"] += float(event.cost)
+
+                    if not event.type_event in events:
+
+                        events.append(event.type_event)
+
+            if titular["total_events"] > 0:
+
+                titular["porcentage_use"] = titular["num_events"] * 100 / titular["total_events"]
+
+            titular["type_events"] = events
+            info['titular'] = titular
 
         if beneficiary:
 
@@ -146,27 +149,35 @@ class ReportMounthly(models.AbstractModel):
             
             beneficiaries["name"] = beneficiary[1]
 
+
             for expedient in all_expedient:
 
                 for beneficiar in expedient.beneficiary_ids:
 
                     if beneficiar.id == beneficiary[0]:
 
-                        beneficiaries["events_total"] = len(beneficiar.event_ids)
+                        beneficiaries["cooperative"] = expedient.cooperative
+                        beneficiaries["relationship"] = beneficiar.relationship
+                        beneficiaries["total_events"] = len(beneficiar.event_ids)
 
                         for event in beneficiar.event_ids:
 
                             #Verificamos si el mes del evento es igual al seleccionado.
                             if compareMounts(month, event.date):
 
-                                beneficiaries["events"] += 1
-                                beneficiaries["mount"] += int(event.cost)
+                                beneficiaries["num_events"] += 1
+                                beneficiaries["mount"] += float(event.cost)
+
+                                if not event.type_event in events:
+
+                                    events.append(event.type_event)
 
 
-            if beneficiaries["events_total"] > 0:
+            if beneficiaries["total_events"] > 0:
 
-                beneficiaries["percentage"] = beneficiaries["events"] * 100 / beneficiaries["events_total"]
+                beneficiaries["porcentage_use"] = beneficiaries["num_events"] * 100 / beneficiaries["total_events"]
 
+            beneficiaries["type_events"] = events
             info['beneficiary'] = beneficiaries
-
+        
         return info
