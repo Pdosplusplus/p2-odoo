d9702a48d9fd2423be3c2d0bd925971837475c58
Victor Pino <victopin0@gmail.com>
2017-08-09 15:19:03 -0400

    Delete the cooperative Leftovers

--- odoo-10/addons/sigesalud/models/expedient.py
+++ odoo-10/addons/sigesalud/models/expedient.py
@@ -17,10 +17,6 @@ class Expedient(models.Model):
         ('Bmkeros', "Bmkeros"),
         ('Vultur', "Vultur"),
         ('Tecno Paraguana', "Tecno Paraguana"),
-        ('Hoatzin', "Hoatzin"),
-        ('Tres Punto Cero', "Tres Punto Cero"),
-        ('Simon Rodriguez', "Simon Rodriguez"),
-        ('Juventud Productiva', "Juventud Productiva"),
         ('Sinapsis', "Sinapsis"),
     ], string="Cooperativa")
 
